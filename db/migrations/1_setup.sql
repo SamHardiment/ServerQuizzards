2a0e844c25af735ecc6bb9a63cd497fdbf11cdfd
Sam Hardiment <samuelghardiment@gmail.com>
2022-06-29 07:51:38 +0100

    add data tables animals, food, random

--- db/migrations/1_setup.sql
+++ db/migrations/1_setup.sql
@@ -5,3 +5,26 @@ CREATE TABLE playesr (
     username varchar(100) NOT NULL,
     points INT 
 );
+
+DROP TABLE IF EXISTS animals;
+
+CREATE TABLE animals (
+    id serial PRIMARY KEY,
+    animal varchar(100) NOT NULL,
+);
+
+
+DROP TABLE IF EXISTS food;
+
+CREATE TABLE food (
+    id serial PRIMARY KEY,
+    animal varchar(100) NOT NULL,
+);
+
+
+DROP TABLE IF EXISTS random;
+
+CREATE TABLE random (
+    id serial PRIMARY KEY,
+    animal varchar(100) NOT NULL,
+);
